d7a14dcdb787e5ee92a57c496b2650f6e9649213
Karikumutima Honore <141430776+karikumutimahonore@users.noreply.github.com>
2024-07-22 18:37:07 +0200

    Adding JSX as an expression

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,28 +1,31 @@
 // Traditionally===html page but in react===Component
 
 const App = () => {
-    //Basic syntax
-    const element = <h2>Hello World</h2>;
-    // Embedding Expressions
-    const name = 'ReactJS Developer';
-    const element2 = (
-        <h1> 
-            Hello {name} {2 + 2}
-        </h1>
-    );
-    //JSX as an Expression 
-    const element3 = (
-        <div>
-            <h1>Hello, World</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
-    return (
-        <div>
-            <h1>Hello, World</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
+  //Basic syntax
+  // eslint-disable-next-line no-unused-vars
+  const element = <h2> Hello World </h2>;
+  // Embedding Expressions
+  const name = "ReactJS Developer";
+  // eslint-disable-next-line no-unused-vars
+  const element2 = (
+    <h1>
+      Hello {name} {2 + 2}{" "}
+    </h1>
+  );
+  //JSX as an Expression
+  // eslint-disable-next-line no-unused-vars
+  const element3 = (
+    <div>
+      <h1>Hello, World</h1> <h2> It is {new Date().toLocaleTimeString()}</h2>{" "}
+    </div>
+  );
+
+  return (
+    <div>
+      <h1>Hello, World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>{" "}
+    </div>
+  );
 };
 
 export default App;
