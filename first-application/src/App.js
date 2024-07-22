63e7984f8f737dc4f1507d960286b32753387f0f
Karikumutima Honore <141430776+karikumutimahonore@users.noreply.github.com>
2024-07-22 23:18:54 +0200

    Applying in-line style-css

--- first-application/src/App.js
+++ first-application/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        src="https://cdn.pixabay.com/photo/2023/07/22/09/04/european-shorthair-8142967_1280.jpg"
+        alt="Avatar"
+        //Apply in-line styling directly
+        style={{
+          width: "150px",
+          height: "150px",
+          borderRadius: "50%",
+          objectFit: "cover",
+        }}
+      />
+      <h2
+        style={{
+          color: "purple",
+        }}
+      >
+        Emmanuel Tweneboah
+      </h2>
+      <p>FullStack Web Developer | React Enthisiast</p>
+      <ul>
+        <li>Coding</li>
+        <li>Hiking</li>
+        <li>Traveling</li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
